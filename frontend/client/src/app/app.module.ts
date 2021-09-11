8fe04b8369c2af64d434504bf4dfb6ac77fb97d8
julien dardennes <jdardennes13170@gmail.com>
2021-09-11 15:08:09 +0200

    mise en place angular

--- frontend/client/src/app/app.module.ts
+++ frontend/client/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import { SignupComponent } from './component/auth/signup/signup.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
