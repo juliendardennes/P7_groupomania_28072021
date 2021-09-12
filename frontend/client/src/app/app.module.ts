4d7c177f34404d5f1ac61e1e4cda33398996dae7
julien dardennes <jdardennes13170@gmail.com>
2021-09-12 21:01:41 +0200

    frontend

--- frontend/client/src/app/app.module.ts
+++ frontend/client/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './component/auth/signup/signup.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
