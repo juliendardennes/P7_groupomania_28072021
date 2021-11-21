86d3892e294ad17839bfa4b248fff70b3261d07c
julien dardennes <jdardennes13170@gmail.com>
2021-11-21 22:35:25 +0100

    debug v2

--- frontend/src/app/composants/text/post-form/post-form.component.ts
+++ frontend/src/app/composants/text/post-form/post-form.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from 'src/app/models/post.model';
 import { AuthService } from 'src/app/service/auth.service';
 import { PostService } from 'src/app/service/post.service';
-import { User } from 'src/app/models/User.model';
 
 @Component({
   selector: 'app-post-form',
@@ -16,32 +15,26 @@ export class PostFormComponent implements OnInit {
   postForm: FormGroup;
   loading: boolean;
   mode: string;
-  user: User;
-  userId: string;
   post: Post;
   errorMsg: string;
 
   constructor(private formBuilder: FormBuilder,
               private route: ActivatedRoute,
               private router: Router,
-              private posts: PostService,
-              private auth: AuthService) { }
+              private posts: PostService) { }
 
   ngOnInit() {
-    this.loading = true;
     this.route.params.subscribe(
       (params) => {
         if (!params.id) {
           this.mode = 'new';
           this.initEmptyForm();
-          this.loading = false;
         } else {
           this.mode = 'edit';
           this.posts.getPostById(params.id).then(
             (post: Post) => {
               this.post = post;
               this.initModifyForm(post);
-              this.loading = false;
             }
           ).catch(
             (error) => {
@@ -55,6 +48,7 @@ export class PostFormComponent implements OnInit {
 
   initEmptyForm() {
     this.postForm = this.formBuilder.group({
+      userId: [null, Validators.required],
       title: [null, Validators.required],
       content: [null, Validators.required],
     });
@@ -62,31 +56,51 @@ export class PostFormComponent implements OnInit {
 
   initModifyForm(post: Post) {
     this.postForm = this.formBuilder.group({
-      title: post.title,
-      content: post.content,
+      userId: [this.post.userId, Validators.required],
+      title: [this.post.title, Validators.required],
+      content: [this.post.content, Validators.required],
     });
   }
 
+  // onSubmit() {
+  //   this.loading = true;
+  //   const newPost = new Post();
+  //   newPost.title = this.postForm.get('title').value;
+  //   newPost.content = this.postForm.get('content').value;
+  //   newPost.userId = JSON.parse(localStorage.getItem("user")).user_id;
+  //   if (this.mode === 'new') {
+  //     this.posts.createPost(newPost.title, newPost.content).then(
+  //       (response: { message: string }) => {
+  //         this.loading = false;
+  //         window.location.reload();
+  //         this.router.navigate(['post-list']);
+  //       }
+  //     ).catch(
+  //       (error) => {
+  //         console.error(error);
+  //         this.loading = false;
+  //         this.errorMsg = error.message;
+  //       }
+  //     );
+  //   } 
+  // }
   onSubmit() {
-    this.loading = true;
-    const newPost = new Post();
-    newPost.title = this.postForm.get('title').value;
-    newPost.content = this.postForm.get('content').value;
-    newPost.userId = JSON.parse(localStorage.getItem("user")).user_id;
+    const title = this.postForm.get('title').value;
+    const content = this.postForm.get('content').value;
+    const userId = JSON.parse(localStorage.getItem("user")).user_id;
     if (this.mode === 'new') {
-      this.posts.createPost(newPost.title, newPost.content).then(
-        (response: { message: string }) => {
-          // window.location.reload();
-          this.loading = false;
+      this.posts.createPost(title,content,userId).then(
+        (response: {message: string }) => {
+          window.location.reload();
           this.router.navigate(['post-list']);
         }
       ).catch(
         (error) => {
           console.error(error);
-          this.loading = false;
           this.errorMsg = error.message;
         }
       );
-    } 
+    }
   }
+  
 }
--- frontend/src/app/service/post.service.ts
+++ frontend/src/app/service/post.service.ts
@@ -10,14 +10,29 @@ import { User } from '../models/User.model';
 })
 export class PostService {
 
-    // posts: Post[] = [];
     post$ = new Subject<Post[]>();
-    id = sessionStorage.getItem('post_id');
-    user_id = sessionStorage.getItem('user_id');
-    user: User;
+    private userId: string;
     
     constructor(private http: HttpClient,
                 private auth: AuthService) {}
+
+    createPost(title:string, content:string, userId:string) {
+      let formData = new FormData();
+      formData.append('title', title);
+      formData.append('content', content);
+      formData.append('user_id',userId)
+        return new Promise((resolve, reject) => {
+           this.http.post('http://localhost:3000/api/posts', formData
+           ).subscribe(
+            (response: { message: string }) => {
+                resolve(response);
+            },
+            (error) => {
+                reject(error);
+            }
+            );
+        });
+    }
 
     getPosts() {
       this.http.get('http://localhost:3000/api/posts').subscribe(
@@ -30,37 +45,6 @@ export class PostService {
         }
       );
     }
-
-    // getPostById(id: string) {
-    //   return new Promise((resolve, reject) => {
-    //     this.http.get('http://localhost:3000/api/posts/' + id).subscribe(
-    //       (post: Post) => {
-    //         resolve(post);
-    //       },
-    //       (error) => {
-    //         reject(error);
-    //       }
-    //     );
-    //   });
-    // }
-
-    createPost(title: string, content: string) {
-        return new Promise((resolve, reject) => {
-           this.http.post('http://localhost:3000/api/posts', {
-              title: title,
-              content: content,
-            }).subscribe(
-            (response: { message: string }) => {
-                resolve(response);
-            },
-            (error) => {
-                reject(error);
-            }
-            );
-        });
-    }
-
-
     
 
     getPostById(id: string) {
